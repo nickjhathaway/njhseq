692789f59d810e954f86d86bc9dc304c1fc0f289
Nicholas Hathaway <nickjhathaway@gmail.com>
2015-10-21 19:21:27 -0400

    a little bit of clean up and adding of copyright;

--- src/bibseq/programUtils/seqSetUp.cpp
+++ src/bibseq/programUtils/seqSetUp.cpp
@@ -1,3 +1,24 @@
+// bibseq - A library for analyzing sequence data
+// Copyright (C) 2012, 2015 Nicholas Hathaway <nicholas.hathaway@umassmed.edu>,
+// Jeffrey Bailey <Jeffrey.Bailey@umassmed.edu>
+//
+// This file is part of bibseq.
+//
+// bibseq is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// bibseq is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with bibseq.  If not, see <http://www.gnu.org/licenses/>.
+//
+//
+
 #include "seqSetUp.hpp"
 #include "bibseq/programUtils/runningParameters.hpp"
 #include <bibcpp/bashUtils.h>
@@ -530,38 +551,38 @@ void seqSetUp::printAdditionaInputUsage(std::ostream& out,
                                         const std::string& lowerRemove) {
   out << bib::bashCT::bold << "Read in processing options: "<< bib::bashCT::reset  << std::endl;
   if (lowerRemove == "remove") {
-    out << "1) -lower [option]: can be remove or upper, remove to remove the "
+    out << "-lower [option]: can be remove or upper, remove to remove the "
            "lower case, upper to convert lower to upper case, defaults to "
            "removing lowercase" << std::endl;
   } else if (lowerRemove == "upper") {
-    out << "1) -lower [option]: can be remove or upper, remove to remove the "
+    out << "-lower [option]: can be remove or upper, remove to remove the "
            "lower case, upper to convert lower to upper case, defaults to "
            "converting to upper case" << std::endl;
   } else {
-    out << "1) -lower [option]: can be remove or upper, remove to remove the "
+    out << "-lower [option]: can be remove or upper, remove to remove the "
            "lower case, upper to convert lower to upper case, defaults to "
            "doing nothing" << std::endl;
   }
-  out << "2) -processed : whether the reads being read have frequency info in "
+  out << "-processed : whether the reads being read have frequency info in "
          "their name, in the form of NAME_t[totalReadNum] or "
          "NAME_f[fractionAmount], defaults to no info" << std::endl;
-  out << "3) -removeGaps : whether to remove any gap infomation (-) in the "
+  out << "-removeGaps : whether to remove any gap information (-) in the "
          "input sequence if there is any, defaults to leaving gaps"
       << std::endl;
-  out << "4) -noSpaceInName,-removeWhiteSpace : wheter to remove any any white "
+  out << "-noSpaceInName,-removeWhiteSpace : wheter to remove any any white "
          "space from the read names or to keep them in, defaults to leaving "
          "white space" << std::endl;
 }
 void seqSetUp::printFileWritingUsage(std::ostream& out, bool all) {
   // std::stringstream tempOut;
   out << bib::bashCT::bold <<"File output options: "<< bib::bashCT::reset << std::endl;
-  out << "1) -out [option]: Name of output file" << std::endl;
-  out << "2) -outFormat [option]: Output file format"
+  out << "-out [option]: Name of output file" << std::endl;
+  out << "-outFormat [option]: Output file format"
          ", options are fasta, fastaQual, and fastq" << std::endl;
   if (all) {
-    out << "3) -overWrite,-overWriteFile : overwrite the file if it "
+    out << "-overWrite,-overWriteFile : overwrite the file if it "
            " already exists, defaults to not overwriting" << std::endl;
-    out << "4) -exitOnFailureToWrite : if fail to write the file whether to "
+    out << "-exitOnFailureToWrite : if fail to write the file whether to "
            "keep going or exit, defaults to keep going " << std::endl;
   }
   // out << cleanOut(tempOut.str(), width_, indent_);
@@ -569,31 +590,25 @@ void seqSetUp::printFileWritingUsage(std::ostream& out, bool all) {
 void seqSetUp::printKmerProfilingUsage(std::ostream& out) {
   // std::stringstream tempOut;
   out << bib::bashCT::bold <<"kmer options"<< bib::bashCT::reset << std::endl;
-  out << "1) -kLength [option]: The length of the k mer check, defaults to " << kLength_ << std::endl;
-  out << "2) -kAnywhere : check any kmers found anywhere, defaults to "
+  out << "-kLength [option]: The length of the k mer check, defaults to " << kLength_ << std::endl;
+  out << "-kAnywhere : check any kmers found anywhere, defaults to "
          "checking for kmers at the position found" << std::endl;
-  out << "3) -runCutOff [option]: kmer occurrence number cut off "
+  out << "-runCutOff [option]: kmer occurrence number cut off "
          "to count as real sequence, defaults to 1, which means it has "
          "to occur in more than one read to be considered real" << std::endl;
   out << "\tif given with a percent sign, will make the cutoff the "
          "percentage, eg. 0.5% has to occur in more than 0.5% of the "
          "reads" << std::endl;
-  out << "4) -qualRunCutOff [option]: kmer occurrence number cut off "
-      << "to raise the quality threshold for mismatches, same formating as "
-         "-runCutOff" << std::endl;
   // out << cleanOut(tempOut.str(), width_, indent_);
 }
 void seqSetUp::printQualThresUsage(std::ostream& out) {
   // std::stringstream tempOut;
   out << bib::bashCT::bold <<"Quality Threshold Options"<< bib::bashCT::reset  << std::endl;
-  out << "1) -qualThres [option]: Quality threshold for high qual "
+  out << "-qualThres [option]: Quality threshold for high qual "
          "mismatch, given in the format of 20,15, where 20 is the "
          "primary quality (of mismatch) and 15 is the secondary quality "
          "(of flanking bases)" << std::endl;
-  out << "2) -qualThresLowKmer [option]: Same as above but this threshold is "
-         "used"
-         " instead if low kmer checking is on, defaults to 30,25" << std::endl;
-  out << "3) -qualThresWindow, -qwindow [option]: Number of flanking qualities "
+  out << "-qualThresWindow, -qwindow [option]: Number of flanking qualities "
          "to "
          "included in quality neighborhood, a value of 5 would mean five "
          "trailing qualities and five"
@@ -622,18 +637,18 @@ void seqSetUp::printAlignmentUsage(std::ostream& out) {
   // std::stringstream tempOut;
   out << bib::bashCT::bold <<"Alignment options"<< bib::bashCT::reset  << std::endl;
   printGapUsage(out);
-  out << "4a) -scoreMatrix : A filename for an alignment scoring matrix "
+  out << "-scoreMatrix : A filename for an alignment scoring matrix "
          "of a custom scoring matrix" << std::endl;
-  out << "4b) -generalMatch [option]: If no score matrix is given the score "
+  out << "-generalMatch [option]: If no score matrix is given the score "
          " of any match will be this, defaults to 2" << std::endl;
-  out << "4c) -generalMismatch [option]: If no score matrix is given the score "
+  out << "-generalMismatch [option]: If no score matrix is given the score "
          " of any mismatch will be this,"
          " defaults to -2" << std::endl;
-  out << "5) -local : do local alignment instead of global, defaults to global"
+  out << "-local : do local alignment instead of global, defaults to global"
       << std::endl;
-  out << "6) -countEndGaps: Whether or not to count end gaps in the "
+  out << "-countEndGaps: Whether or not to count end gaps in the "
          "alignment comparison" << std::endl;
-  out << "7) -noHomopolymerWeighting,-noHWeighting: In alignment comparison, "
+  out << "-noHomopolymerWeighting,-noHWeighting: In alignment comparison, "
          "do not count"
          " indels in homopolymer differnt from other indels" << std::endl;
   // out << cleanOut(tempOut.str(), width_, indent_);
@@ -647,7 +662,7 @@ void seqSetUp::printReferenceComparisonUsage(std::ostream& out) {
   out << "1b) -refFastq,-expectFastq [option]: Same as above but if the file "
          "is in"
          " fastq format " << std::endl;
-  out << "2) -refprocessed,-expectedProcessed : If the references have "
+  out << "-refprocessed,-expectedProcessed : If the references have "
          "frequency info"
          " in their name, in the form of _t[readNumber] or _f[Fraction]"
       << std::endl;
@@ -656,12 +671,12 @@ void seqSetUp::printReferenceComparisonUsage(std::ostream& out) {
 void seqSetUp::printAlnInfoDirUsage(std::ostream& out) {
   // std::stringstream tempOut;
   out << bib::bashCT::bold <<"Save Alignments Options: "<< bib::bashCT::reset  << std::endl;
-  out << "1) -alnInfoDir [option] : Name of a directory to save alingments"
+  out << "-alnInfoDir [option] : Name of a directory to save alingments"
          " if the directory already exists it assumes it has saved alingments "
          "and these will be read in and used, if the directory does not exist "
          "a new directory will be created with this name and alingment will be "
          "save here" << std::endl;
-  out << "2) -outAlnInfoDir,-outAln,-outAlnInfo [option] : Name of a new "
+  out << "-outAlnInfoDir [option] : Name of a new "
          "directory to save alignments in, if -alnInfoDir given but nothing "
          "for this option, the out directory will default to the -alnInfodir "
          "name" << std::endl;
@@ -671,13 +686,10 @@ void seqSetUp::printAlnInfoDirUsage(std::ostream& out) {
 void seqSetUp::printAdditionalClusteringUsage(std::ostream& out) {
   // std::stringstream tempOut;
   out << bib::bashCT::bold <<"Optional Clustering options"<< bib::bashCT::reset << std::endl;
-  out << "1) -bestMatch : Has the program cluster the reads looking "
-         "for the best match, defaults to the first read that meets "
-         "the given parameters" << std::endl;
-  out << "2) -bestMatchCheck [option] : The number of reads to look "
+  out << "-bestMatchCheck [option] : The number of reads to look "
          "for when -bestMatch has been switched on, defaults to 10"
       << std::endl;
-  out << "3) -largestFirst : Cluster the reads by comparing the "
+  out << "-largestFirst : Cluster the reads by comparing the "
          "largest clusters to each other first, defaults to taking the "
          "smallest cluster and comparing to the largest and making "
          "it's way up" << std::endl;
