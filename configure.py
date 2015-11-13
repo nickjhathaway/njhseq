f4f6aca81598d90381fa0f295896e0f989c6a87c
Nicholas Hathaway <nickjhathaway@gmail.com>
2015-11-13 13:34:26 -0500

    update for bibcpp version;

--- configure.py
+++ configure.py
@@ -7,7 +7,7 @@ from utils import Utils
 from genFuncs import genHelper
 def main():
     name = "bibseq"
-    libs = "cppitertools,boost,bamtools,curl,bibcpp:release/2.2.1,armadillo"
+    libs = "cppitertools,boost,bamtools,curl,bibcpp:2.2.1,armadillo"
     args = genHelper.parseNjhConfigureArgs()
     cmd = genHelper.mkConfigCmd(name, libs, sys.argv)
     Utils.run(cmd)
