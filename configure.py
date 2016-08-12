4292203ba3b987a03887c97c16eca5ea9126113b
Nicholas Hathaway <nickjhathaway@gmail.com>
2016-08-12 00:49:22 -0400

    no message

--- configure.py
+++ configure.py
@@ -7,13 +7,13 @@ from utils import Utils
 from genFuncs import genHelper
 def main():
     name = "bibseq"
-    libs = "bamtools:v2.4.0,bibcpp:release/v2.4.0,armadillo:6.200.3,TwoBit:release/v2.0.4,hts:1.3.1"
+    libs = "bamtools:v2.4.0,bibcpp:v2.4.0,armadillo:6.200.3,TwoBit:v2.0.4,hts:1.3.1"
     args = genHelper.parseNjhConfigureArgs()
     if Utils.isMac():
         if args.CC and "gcc" in args.CC[0]:
             pass
         else:
-            libs = libs + ",sharedMutex:release/v0.6"
+            libs = libs + ",sharedMutex:v0.6"
     cmd = genHelper.mkConfigCmd(name, libs, sys.argv, "-lcurl")
     Utils.run(cmd)
 main()
