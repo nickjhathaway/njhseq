4ae0e9c8296b6ae60bb1f5ff1958ae2547e4d655
Nicholas Hathaway <nickjhathaway@gmail.com>
2017-01-25 11:37:47 -0500

    version bump;

--- configure.py
+++ configure.py
@@ -7,7 +7,7 @@ from utils import Utils
 from genFuncs import genHelper
 def main():
     name = "bibseq"
-    libs = "bamtools:develop,bibcpp:develop,armadillo:7.600.1"
+    libs = "bamtools:release/2.5.0,bibcpp:release/2.5.0,armadillo:7.600.1"
     args = genHelper.parseNjhConfigureArgs()
     if Utils.isMac():
         macv, _, _ = platform.mac_ver()
@@ -16,7 +16,7 @@ def main():
             if args.CC and "gcc" in args.CC[0]:
                 pass
             else:
-                libs = libs + ",sharedMutex:develop"
+                libs = libs + ",sharedMutex:release/v0.7"
     cmd = genHelper.mkConfigCmd(name, libs, sys.argv)
     
     Utils.run(cmd)
