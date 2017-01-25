01ba8b89e0b97ab7c125d9b7ae08d32b7dfee6c5
Nicholas Hathaway <nickjhathaway@gmail.com>
2017-01-25 14:15:05 -0500

    no message

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 bibseq
 ======
-Version 2.5.0
+Version 2.5.1-dev
 
 C++ library for dealing with dna sequence data and various other biological data create by students at Umass Medical School 
 
--- configure.py
+++ configure.py
@@ -7,7 +7,7 @@ from utils import Utils
 from genFuncs import genHelper
 def main():
     name = "bibseq"
-    libs = "bamtools:v2.5.0,bibcpp:v2.5.0,armadillo:7.600.1"
+    libs = "bamtools:develop,bibcpp:develop,armadillo:7.600.1"
     args = genHelper.parseNjhConfigureArgs()
     if Utils.isMac():
         macv, _, _ = platform.mac_ver()
@@ -16,7 +16,7 @@ def main():
             if args.CC and "gcc" in args.CC[0]:
                 pass
             else:
-                libs = libs + ",sharedMutex:v0.7"
+                libs = libs + ",sharedMutex:develop"
     cmd = genHelper.mkConfigCmd(name, libs, sys.argv)
     
     Utils.run(cmd)
