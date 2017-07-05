8c0544e5f2fbb1d3419845fea207e9f1ff082efc
Nicholas Hathaway <nickjhathaway@gmail.com>
2017-07-05 01:58:15 -0400

    more debugging;

--- src/bibseq/system/BioCmdsUtils.cpp
+++ src/bibseq/system/BioCmdsUtils.cpp
@@ -308,6 +308,7 @@ BioCmdsUtils::FastqDumpResults BioCmdsUtils::runFastqDump(const FastqDumpPars & 
 						checkFile1) };
 				firstMateIoOpts.out_.overWriteFile_ = true;
 				gzFirstMateTh = std::make_unique<std::thread>([&firstMateIoOpts]() {
+					bib::GZSTREAM::ogzstream out;
 					//gzZipFile(firstMateIoOpts);
 					bfs::remove(firstMateIoOpts.in_.inFilename_);
 				});
