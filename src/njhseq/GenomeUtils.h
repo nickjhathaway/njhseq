5e30b048481c3b8dd040f88f077e51065ee9c4e7
Nick Hathaway <nickjhathaway@gmail.com>
2019-05-26 19:01:46 -0400

    added genome region generator for making parallel processing easier;

--- src/njhseq/GenomeUtils.h
+++ src/njhseq/GenomeUtils.h
@@ -30,4 +30,4 @@
 #include "njhseq/GenomeUtils/GenomeExtraction.h"
 #include "njhseq/GenomeUtils/GenomeMapping.h"
 #include "njhseq/GenomeUtils/GenomeUtils.hpp"
-
+#include "njhseq/GenomeUtils/GenomeRegionsGenerator.hpp"
